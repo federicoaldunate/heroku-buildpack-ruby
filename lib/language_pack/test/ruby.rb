564fa336c0eee1dfe58da6d01aa4416d2242f59f
Isa14 <isa.ivagnes@gmail.com>
2019-11-05 09:30:43 -0300

    Run assets precompile command

--- lib/language_pack/test/ruby.rb
+++ lib/language_pack/test/ruby.rb
@@ -16,6 +16,7 @@ class LanguagePack::Ruby
         build_bundler(bundle_path: "vendor/bundle", default_bundle_without: "development")
         post_bundler
         install_binaries
+        run_assets_precompile_rake_task
       end
       super
     end
