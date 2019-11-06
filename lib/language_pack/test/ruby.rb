422e1b541d03c2b6ce51e3b1f951f7ea8881bdbd
Isa14 <isa.ivagnes@gmail.com>
2019-11-06 10:43:19 -0300

    Create database.yml

--- lib/language_pack/test/ruby.rb
+++ lib/language_pack/test/ruby.rb
@@ -14,6 +14,7 @@ class LanguagePack::Ruby
         install_bundler_in_app
         build_bundler("development")
         post_bundler
+        create_database_yml
         install_binaries
         prepare_tests
         run_assets_precompile_rake_task
