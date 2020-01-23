b19dac7bda6acc802499a5b01d850d7878aacf2b
lucasocon <lucas_masterclas@hotmail.com>
2020-01-23 11:23:54 -0400

    revert changes, delete test/rails51.rb

--- lib/language_pack/test/rails51.rb
+++
@@ -1,7 +0,0 @@
-class LanguagePack::Rails51
-  private
-  def store_cache?
-    # only if it is the last dyno of the CI formation
-    env('CI_NODE_INDEX').to_i == (env('CI_NODE_TOTAL').to_i - 1)
-  end
-end
