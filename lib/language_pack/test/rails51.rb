7097138966bd2aaeba8a2db67ba380b09979018b
lucasocon <lucas_masterclas@hotmail.com>
2020-01-23 10:39:12 -0400

    fix test/rails51.rb

--- lib/language_pack/test/rails51.rb
+++ lib/language_pack/test/rails51.rb
@@ -1,4 +1,4 @@
-class LanguagePack::Ruby
+class LanguagePack::Rails51
   def store_cache?
     # only if it is the last dyno of the CI formation
     env('CI_NODE_INDEX').to_i == (env('CI_NODE_TOTAL').to_i - 1)
